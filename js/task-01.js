5c7760d00fae015cf5f43b82f8c88648e11d3d70
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-12 20:45:38 +0300

    hm

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,9 @@ const allCategories = document.querySelectorAll(".item");
 console.log(`В списке ${allCategories.length} категории`);
 
 const categoriesArray = [...allCategories]
-    .map((el) => `Категория: ${el.children[0].textContent}\nКоличество элементов: ${el.children[1].children.length}`)
-    .join("\n")
+  .map(
+    (el) =>
+      `Категория: ${el.children[0].textContent}\nКоличество элементов: ${el.children[1].children.length}`
+  )
+  .join("\n");
 console.log(categoriesArray);
---
+++ js/task-02.js
@@ -0,0 +1,35 @@
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+const claster = ingredients.method((item) => {
+  console.log(item);
+});
+// const list = document.querySelector("#ingredients");
+// list.appendChild(item);
+
+// Создадим еще один li и вставим его в конец списка
+// const item = document.createElement("a");
+// item.href = "#";
+// item.classList.add("btn");
+// item.textContent = "item 4";
+
+// const nav = document.querySelector(".nav");
+// nav.appendChild(item);
+
+// Создадим заголовок и вставим его в container перед списком
+// const heading = document.createElement("h1");
+// heading.textContent = "Nav heading";
+
+// const container = document.querySelector(".container");
+// container.insertBefore(heading, nav);
+
+// console.log(ingredients);
+
+// for (let i; i > 1; i++) {
+
+// }
