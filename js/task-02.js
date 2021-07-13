3a039486d3e2a80c3809bf9ec96c1c558aed8dd8
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-13 10:40:23 +0300

    task №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,30 +6,12 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
-const claster = ingredients.method((item) => {
-  console.log(item);
+
+const listArr = document.querySelector("#ingredients");
+const elements = ingredients.map((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = element;
+  return itemEl;
 });
-// const list = document.querySelector("#ingredients");
-// list.appendChild(item);
-
-// Создадим еще один li и вставим его в конец списка
-// const item = document.createElement("a");
-// item.href = "#";
-// item.classList.add("btn");
-// item.textContent = "item 4";
-
-// const nav = document.querySelector(".nav");
-// nav.appendChild(item);
-
-// Создадим заголовок и вставим его в container перед списком
-// const heading = document.createElement("h1");
-// heading.textContent = "Nav heading";
-
-// const container = document.querySelector(".container");
-// container.insertBefore(heading, nav);
-
-// console.log(ingredients);
-
-// for (let i; i > 1; i++) {
-
-// }
+console.log(elements);
+listArr.append(...elements);
