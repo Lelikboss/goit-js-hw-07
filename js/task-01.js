7ff18b1891108d811680a651fabb106067a13ddd
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-14 22:59:57 +0300

    fix

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,17 @@
-const allCategories = document.querySelectorAll(".item");
-console.log(`В списке ${allCategories.length} категории`);
-const arrayCategories = [...allCategories].map(
-  (category) =>
-    `Категория: ${category.children[0].textContent}\nКоличество элементов: ${category.children[1].children.length}`
-);
-console.log(arrayCategories.join("\n"));
+// const allCategories = document.querySelectorAll(".item");
+// console.log(`В списке ${allCategories.length} категории`);
+// const arrayCategories = [...allCategories].map(
+//   (category) =>
+//     `Категория: ${category.children[0].textContent}\nКоличество элементов: ${category.children[1].children.length}`
+// );
+// console.log(arrayCategories.join("\n"));
+
+const categoriesListEl = document.querySelectorAll(".item");
+console.log(`В списке ${categoriesListEl.length} категории`);
+const showCategories = (element) =>
+  element.forEach((el) =>
+    console.log(
+      `Категория: ${el.firstElementChild.textContent}.\nКоличество элементов: ${el.lastElementChild.children.length}.`
+    )
+  );
+showCategories(categoriesListEl);
