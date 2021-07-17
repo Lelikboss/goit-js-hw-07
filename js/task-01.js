3739d4da0e26b1249d054d8ef1a750dcfa014feb
Alexey Alexeew <lelikboss97@gmail.com>
2021-07-17 09:47:31 +0300

    add 'use strict'

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,4 @@
-// const allCategories = document.querySelectorAll(".item");
-// console.log(`В списке ${allCategories.length} категории`);
-// const arrayCategories = [...allCategories].map(
-//   (category) =>
-//     `Категория: ${category.children[0].textContent}\nКоличество элементов: ${category.children[1].children.length}`
-// );
-// console.log(arrayCategories.join("\n"));
+"use strict";
 
 const categoriesListEl = document.querySelectorAll(".item");
 console.log(`В списке ${categoriesListEl.length} категории`);
